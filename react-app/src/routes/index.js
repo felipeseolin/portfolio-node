604f981d8b257e5432ec4d856b7ce5624c450e4b
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-06-27 02:01:46 -0300

    New routes on react

--- react-app/src/routes/index.js
+++ react-app/src/routes/index.js
@@ -1,17 +1,31 @@
-import React, { Component } from "react";
-import { BrowserRouter, Switch, Route, HashRouter } from "react-router-dom";
+import React, {Component} from "react";
+import {BrowserRouter, Switch, Route, HashRouter} from "react-router-dom";
 
 import Main from "../pages/Main";
 import Blog from "../pages/Blog";
+import Api from "../pages/Api";
+// Biography
+import Biography from "../pages/Api/Biography";
+import BiographyEdit from "../pages/Api/Biography/edit";
 
-class Router extends Component{
+class Router extends Component {
     render() {
         return (
             <HashRouter basename="/">
-                    <Route exact path="#" component={Main} />
-                    <Route exact path="/" component={Main} />
-                    <Route exact path="/blog/:id" component={Blog} />
-
+                <Route exact path="#" component={Main}/>
+                <Route exact path="/" component={Main}/>
+                <Route exact path="/blog/:id" component={Blog}/>
+                {/* API */}
+                <Route exact path="/api" component={Api}/>
+                {/* Biography pages */}
+                <Route exact path="/api/biography" component={Biography}/>
+                {/*<Route exact path="/api/biography/create" component={BiographyCreate}/>*/}
+                <Route exact path="/api/biography/:id" component={BiographyEdit}/>
+                {/* */}
+                <Route exact path="/api" component={Api}/>
+                <Route exact path="/api" component={Api}/>
+                <Route exact path="/api" component={Api}/>
+                {/* */}
             </HashRouter>
         );
     }
